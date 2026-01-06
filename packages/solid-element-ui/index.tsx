08adf81674860a6ae181afb75e8aa645c0935e7e
MasterYu <2279442929@qq.com>
2026-01-06 12:52:12 +0800

    add

--- packages/solid-element-ui/index.tsx
+++ packages/solid-element-ui/index.tsx
@@ -9,89 +9,89 @@
 // TODO 格栅布局，flex布局，space看情况再说添加不添加
 
 import "../css/index.css";
-export { SeButton } from "solid-element-ui/components/button/src/button";
-export { SeButtonGroup } from "solid-element-ui/components/button/src/button-group";
-export { SeAffix } from "solid-element-ui/components/affix/src/affix";
-export { SeBreadcrumb } from "solid-element-ui/components/breadcrumb/src/breadcrumb";
-export { SeBreadcrumbItem } from "solid-element-ui/components/breadcrumb/src/breadcrumb-item";
+export { SeButton } from "@solid-element-ui/components/button/src/button";
+export { SeButtonGroup } from "@solid-element-ui/components/button/src/button-group";
+export { SeAffix } from "@solid-element-ui/components/affix/src/affix";
+export { SeBreadcrumb } from "@solid-element-ui/components/breadcrumb/src/breadcrumb";
+export { SeBreadcrumbItem } from "@solid-element-ui/components/breadcrumb/src/breadcrumb-item";
 
-export { SeAnchor } from "solid-element-ui/components/anchor/src/anchor";
-export { SeAnchorLink } from "solid-element-ui/components/anchor/src/anchor-link";
+export { SeAnchor } from "@solid-element-ui/components/anchor/src/anchor";
+export { SeAnchorLink } from "@solid-element-ui/components/anchor/src/anchor-link";
 
-export { SeDivider } from "solid-element-ui/components/divider/src/divider";
+export { SeDivider } from "@solid-element-ui/components/divider/src/divider";
 
-export { SeAutocomplete } from "solid-element-ui/components/autocomplete/src/autocomplete";
+export { SeAutocomplete } from "@solid-element-ui/components/autocomplete/src/autocomplete";
 
-export { SeCascader } from "solid-element-ui/components/cascader/src/cascader";
+export { SeCascader } from "@solid-element-ui/components/cascader/src/cascader";
 
-export { SeCheckbox } from "solid-element-ui/components/checkbox/src/checkbox";
+export { SeCheckbox } from "@solid-element-ui/components/checkbox/src/checkbox";
 
-export { SeColorPicker } from "solid-element-ui/components/color-picker/src/color-picker";
+export { SeColorPicker } from "@solid-element-ui/components/color-picker/src/color-picker";
 
-export { SeDatePicker } from "solid-element-ui/components/date-picker/src/date-picker";
+export { SeDatePicker } from "@solid-element-ui/components/date-picker/src/date-picker";
 
-export { SeForm } from "solid-element-ui/components/form/src/form";
-export { SeFormItem } from "solid-element-ui/components/form/src/form-item";
+export { SeForm } from "@solid-element-ui/components/form/src/form";
+export { SeFormItem } from "@solid-element-ui/components/form/src/form-item";
 
 
-export { SeInput } from "solid-element-ui/components/input/src/input";
-export { SeInputNumber } from "solid-element-ui/components/input-number/src/input-number";
-export { SeInputTag } from "solid-element-ui/components/input-tag/src/input-tag";
-export { SeMentions } from "solid-element-ui/components/mentions/src/mentions";
+export { SeInput } from "@solid-element-ui/components/input/src/input";
+export { SeInputNumber } from "@solid-element-ui/components/input-number/src/input-number";
+export { SeInputTag } from "@solid-element-ui/components/input-tag/src/input-tag";
+export { SeMentions } from "@solid-element-ui/components/mentions/src/mentions";
 
-export { SeRadio } from "solid-element-ui/components/radio/src/radio";
-export { SeRadioGroup } from "solid-element-ui/components/radio/src/radio-group";
-export { SeRate } from "solid-element-ui/components/rate/src/rate";
-export { SeSelect } from "solid-element-ui/components/select/src/select";
-export { SeSlider } from "solid-element-ui/components/slider/src/slider";
-export { SeSwitch } from "solid-element-ui/components/switch/src/switch";
-export { SeTimePicker } from "solid-element-ui/components/time-picker/src/time-picker";
-export { SeTransfer } from "solid-element-ui/components/transfer/src/transfer";
-export { SeTreeSelect } from "solid-element-ui/components/tree-select/src/tree-select";
-export { SeUpload } from "solid-element-ui/components/upload/src/upload";
-export { type UploadFile } from "solid-element-ui/components/upload/src/setting.ts";
-export { SeAvatar } from "solid-element-ui/components/avatar/src/avatar";
-export { SeBadge } from "solid-element-ui/components/badge/src/badge";
-export { SeCalendar } from "solid-element-ui/components/calendar/src/calendar";
-export { SeCard } from "solid-element-ui/components/card/src/card";
-export { SeCarousel } from "solid-element-ui/components/carousel/src/carousel";
-export { SeCollapse } from "solid-element-ui/components/collapse/src/collapse";
-export { SeCollapsePanel } from "solid-element-ui/components/collapse/src/collapse-panel";
-export { SeDescription } from "solid-element-ui/components/description/src/description";
-export { SeDescriptionItem } from "solid-element-ui/components/description/src/description-item";
-export { SeEmpty } from "solid-element-ui/components/empty/src/empty";
-export { SeImage } from "solid-element-ui/components/image/src/image";
-export { SePopover } from "solid-element-ui/components/popover/src/popover";
-export { SeSegmented } from "solid-element-ui/components/segmented/src/segmented";
-export { SeStatistic } from "solid-element-ui/components/statistic/src/statistic";
-export { SeTable } from "solid-element-ui/components/table/src/table";
-export { type ColumnType } from "solid-element-ui/components/table/src/setting.ts";
-export { SeTag } from "solid-element-ui/components/tag/src/tag";
-export { SeTimeline } from "solid-element-ui/components/timeline/src/timeline";
-export { SeTimelineItem } from "solid-element-ui/components/timeline/src/timeline-item";
-export { SeTooltip } from "solid-element-ui/components/tooltip/src/tooltip";
-export { SeTour } from "solid-element-ui/components/tour/src/tour";
-export { SeTree } from "solid-element-ui/components/tree/src/tree";
-export { type TreeData } from "solid-element-ui/components/tree/src/setting.ts";
-export { SeAlert } from "solid-element-ui/components/alert/src/alert";
-export { SeDrawer } from "solid-element-ui/components/drawer/src/drawer";
-export { SeMessage } from "solid-element-ui/components/message/src/message";
-export { SeModal } from "solid-element-ui/components/modal/src/modal";
-export { SeNotification } from "solid-element-ui/components/notification/src/notification";
-export { SePopconfirm } from "solid-element-ui/components/popconfirm/src/popconfirm";
-export { SeProgress } from "solid-element-ui/components/progress/src/progress";
-export { SeResult } from "solid-element-ui/components/result/src/result";
-export { SeSkeleton } from "solid-element-ui/components/skeleton/src/skeleton";
-export { SeSkeletonElement } from "solid-element-ui/components/skeleton/src/skeleton";
-export { SeSpin } from "solid-element-ui/components/spin/src/spin";
-export { SeWatermark } from "solid-element-ui/components/watermark/src/watermark";
-export { SeDropdown } from "solid-element-ui/components/dropdown/src/dropdown";
-export { SeMenu } from "solid-element-ui/components/menu/src/menu";
-export { type MenuItemType } from "solid-element-ui/components/menu/src/setting.ts";
-export { SePagination } from "solid-element-ui/components/pagination/src/pagination";
-export { SeSteps } from "solid-element-ui/components/steps/src/steps";
-export { SeTabs } from "solid-element-ui/components/tabs/src/tabs";
-export { SeMasonry } from "solid-element-ui/components/masonry/src/masonry";
+export { SeRadio } from "@solid-element-ui/components/radio/src/radio";
+export { SeRadioGroup } from "@solid-element-ui/components/radio/src/radio-group";
+export { SeRate } from "@solid-element-ui/components/rate/src/rate";
+export { SeSelect } from "@solid-element-ui/components/select/src/select";
+export { SeSlider } from "@solid-element-ui/components/slider/src/slider";
+export { SeSwitch } from "@solid-element-ui/components/switch/src/switch";
+export { SeTimePicker } from "@solid-element-ui/components/time-picker/src/time-picker";
+export { SeTransfer } from "@solid-element-ui/components/transfer/src/transfer";
+export { SeTreeSelect } from "@solid-element-ui/components/tree-select/src/tree-select";
+export { SeUpload } from "@solid-element-ui/components/upload/src/upload";
+export { type UploadFile } from "@solid-element-ui/components/upload/src/setting.ts";
+export { SeAvatar } from "@solid-element-ui/components/avatar/src/avatar";
+export { SeBadge } from "@solid-element-ui/components/badge/src/badge";
+export { SeCalendar } from "@solid-element-ui/components/calendar/src/calendar";
+export { SeCard } from "@solid-element-ui/components/card/src/card";
+export { SeCarousel } from "@solid-element-ui/components/carousel/src/carousel";
+export { SeCollapse } from "@solid-element-ui/components/collapse/src/collapse";
+export { SeCollapsePanel } from "@solid-element-ui/components/collapse/src/collapse-panel";
+export { SeDescription } from "@solid-element-ui/components/description/src/description";
+export { SeDescriptionItem } from "@solid-element-ui/components/description/src/description-item";
+export { SeEmpty } from "@solid-element-ui/components/empty/src/empty";
+export { SeImage } from "@solid-element-ui/components/image/src/image";
+export { SePopover } from "@solid-element-ui/components/popover/src/popover";
+export { SeSegmented } from "@solid-element-ui/components/segmented/src/segmented";
+export { SeStatistic } from "@solid-element-ui/components/statistic/src/statistic";
+export { SeTable } from "@solid-element-ui/components/table/src/table";
+export { type ColumnType } from "@solid-element-ui/components/table/src/setting.ts";
+export { SeTag } from "@solid-element-ui/components/tag/src/tag";
+export { SeTimeline } from "@solid-element-ui/components/timeline/src/timeline";
+export { SeTimelineItem } from "@solid-element-ui/components/timeline/src/timeline-item";
+export { SeTooltip } from "@solid-element-ui/components/tooltip/src/tooltip";
+export { SeTour } from "@solid-element-ui/components/tour/src/tour";
+export { SeTree } from "@solid-element-ui/components/tree/src/tree";
+export { type TreeData } from "@solid-element-ui/components/tree/src/setting.ts";
+export { SeAlert } from "@solid-element-ui/components/alert/src/alert";
+export { SeDrawer } from "@solid-element-ui/components/drawer/src/drawer";
+export { SeMessage } from "@solid-element-ui/components/message/src/message";
+export { SeModal } from "@solid-element-ui/components/modal/src/modal";
+export { SeNotification } from "@solid-element-ui/components/notification/src/notification";
+export { SePopconfirm } from "@solid-element-ui/components/popconfirm/src/popconfirm";
+export { SeProgress } from "@solid-element-ui/components/progress/src/progress";
+export { SeResult } from "@solid-element-ui/components/result/src/result";
+export { SeSkeleton } from "@solid-element-ui/components/skeleton/src/skeleton";
+export { SeSkeletonElement } from "@solid-element-ui/components/skeleton/src/skeleton";
+export { SeSpin } from "@solid-element-ui/components/spin/src/spin";
+export { SeWatermark } from "@solid-element-ui/components/watermark/src/watermark";
+export { SeDropdown } from "@solid-element-ui/components/dropdown/src/dropdown";
+export { SeMenu } from "@solid-element-ui/components/menu/src/menu";
+export { type MenuItemType } from "@solid-element-ui/components/menu/src/setting.ts";
+export { SePagination } from "@solid-element-ui/components/pagination/src/pagination";
+export { SeSteps } from "@solid-element-ui/components/steps/src/steps";
+export { SeTabs } from "@solid-element-ui/components/tabs/src/tabs";
+export { SeMasonry } from "@solid-element-ui/components/masonry/src/masonry";
 
 
 const justForTest = () => {
